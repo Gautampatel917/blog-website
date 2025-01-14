0d1dffe43907f7e41b77352df49f71211f50e589
Gautam <wwwgautampatel3@gmail.com>
2025-01-14 10:54:48 +0530

    complete home page

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,23 +2,17 @@ import { Link } from 'react-router-dom';
 
 export default function PostCard({ post }) {
     return (
-        <div className='group relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[430px] transition-all'>
+        <div className='w-full md:w-1/2 xl:w-1/3 p-6'>
             <Link to={`/post/${post.slug}`}>
                 <img
                     src={post.image}
                     alt='post cover'
-                    className='h-[260px] w-full  object-cover group-hover:h-[200px] transition-all duration-300 z-20'
+                    className='h-48 w-full object-cover'
                 />
             </Link>
-            <div className='p-3 flex flex-col gap-2'>
-                <p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
-                <span className='italic text-sm'>{post.category}</span>
-                <Link
-                    to={`/post/${post.slug}`}
-                    className='z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
-                >
-                    Read article
-                </Link>
+            <div className='pt-3'>
+                <h2 className='text-lg font-bold'>{post.title}</h2>
+                <p className='text-gray-500'>{post.category}</p>
             </div>
         </div>
     );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,59 @@
-export default function Home(){
+import { Link } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
+import { useEffect, useState } from 'react';
+import PostCard from '../components/PostCard';
+
+export default function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await fetch('/api/post/getPost');
+            const data = await res.json();
+            setPosts(data.posts);
+        };
+        fetchPosts();
+    }, []);
     return (
-        <div className="home">
-            <h1>Home</h1>
+        <div>
+            <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+                <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+                <p className='text-gray-500 text-xs sm:text-sm'>
+                    Here you'll find a variety of articles and tutorials on topics such as
+                    web development, software engineering, and programming languages.
+                </p>
+                <Link
+                    to='/search'
+                    className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+                >
+                    View all posts
+                </Link>
+            </div>
+            <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+                <CallToAction />
+            </div>
+
+            <div className='recent-posts-container'>
+                {posts && posts.length > 0 && (
+                    <div className='recent-posts mt-8'>
+                        <h1 className='text-3xl'>Recent Posts</h1>
+                        <div className='post-cards'>
+                            {posts.slice(0, 6).map((post) => (
+                                <PostCard key={post._id} post={post} />
+                            ))}
+                        </div>
+                        {posts.length > 6 && (
+                            <Link
+                                to={'/search'}
+                                className='view-all-posts text-lg
+                                text-teal-500 hover:underline text-center'
+                            >
+                                View all posts
+                            </Link>
+                        )}
+                    </div>
+                )}
+            </div>
         </div>
-    )
+    );
 }
