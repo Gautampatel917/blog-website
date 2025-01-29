2c05d8a11f2a70cb7e58d71e92c51ee95710a502
Gautam <wwwgautampatel3@gmail.com>
2025-01-29 20:51:57 +0530

    user route error solved successfully

--- api/routes/user.router.js
+++ api/routes/user.router.js
@@ -5,19 +5,11 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 // Default API route
-router.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json({ message: "API is working..." });
-});
-
-// Test route
 router.get('/test', test);
-
-// Protected route: update user
-router.get('/getusers', verifyToken, getUsers)
-router.get('/:userId', verifyToken, getUser);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
